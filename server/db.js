dd80cb2a5f284e3ea2bfa8eb7c21b307565c7ab4
Veerengiri Goswami <95561796+Veerengiri@users.noreply.github.com>
2024-04-19 22:21:36 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongourl = "mongodb+srv://Veerengiri:762003@cluster0.vjo3ghd.mongodb.net/hotel?retryWrites=true&w=majority"
+const mongourl = ""
 
 mongoose.connect(mongourl,{}).then(()=>{
     console.log("connected mongourl successfully");
